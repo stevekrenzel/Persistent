d6ff5fa1ff8de59ba67d262fac584fbce5be7a65
Steve Krenzel <sgk284@gmail.com>
2009-05-29 19:22:44 -0400

    Adding integer list

---
+++ IntegerList/integer_list.py
@@ -0,0 +1,6 @@
+from PersistentList import PersistentList
+
+class IntegerList(PersistentList):
+    def __init__(self, file_object, number_of_ints=1, address=0):
+        PersistentList.__init__(self, file_object, "I"*number_of_ints,
+                address=address)
