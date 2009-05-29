3b797006a893caea24645a7b080682d49dc6adcf
Steve Krenzel <sgk284@gmail.com>
2009-05-29 19:36:21 -0400

    Removing integer list

--- integer_list.py
+++
@@ -1,6 +0,0 @@
-from PersistentList import PersistentList
-
-class IntegerList(PersistentList):
-    def __init__(self, file_object, number_of_ints=1, address=0):
-        PersistentList.__init__(self, file_object, "I"*number_of_ints,
-                address=address)
