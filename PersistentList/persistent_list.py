119d0d0d5b19ce44a422c36d452b6f04f60a6e13
Steve Krenzel <sgk284@gmail.com>
2009-05-29 13:35:58 -0400

    Initial commit

---
+++ PersistentList/persistent_list.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+############################################
+# Released in the Public Domain, do with it
+# as you please.
+#
+# Author: Steve Krenzel (sgk284@gmail.com)
+############################################
+
+from struct import pack, unpack, calcsize
+
+class PersistentList:
+    def __init__(self, file_object, value_format, address=None):
+        self.file_object  = file_object
+        self.value_format = value_format
+        self.head         = None
+        self.tail         = None
+        if address != None and address != 0:
+            self.head    = PersistentNode(self.file_object, self.value_format, address=address)
+            self.tail    = None
+            for node in self.node_iter():
+                self.tail = node
+
+    def append(self, *value):
+        new_node = PersistentNode(self.file_object, self.value_format, value)
+        if self.head == None:
+            self.head = new_node
+        else:
+            self.tail.set_next(new_node)
+        self.tail = new_node
+
+    def node_iter(self):
+        current = self.head
+        while current is not None:
+            yield current
+            current = current.get_next()
+
+    def __iter__(self):
+        for i in self.node_iter():
+            yield i.get_value()
+
+    def __str__(self):
+        return "[" + ", ".join([str(i) for i in self]) + "]"
+
+class PersistentNode:
+    def __init__(self, file_object, value_format, value = None, next = None, address=None):
+        self.file_object  = file_object
+        self.value_format = value_format
+        self.value_length = calcsize(self.value_format)
+        self.data_format  = self.value_format + "I" # Add pointer 
+        self.data_length  = calcsize(self.data_format)
+        if address == None or address == 0:
+            self.file_object.seek(0, 2)
+            value = [0]*self.value_length if value is None else list(value)
+            value.append(0 if next is None else next.address)
+            self.file_object.write(pack(self.data_format, *value))
+            self.address = self.file_object.tell()
+        else:
+            self.address = address
+
+    def set_value(self, *value):
+        data = self.get_data() 
+        data = list(value) + data[-1]
+        self.set_bytes(data)
+
+    def set_next(self, next):
+        data = self.get_data() 
+        data[-1] = next.address
+        self.set_data(data)
+
+    def set_data(self, data):
+        self.file_object.seek(self.address - self.data_length)
+        self.file_object.write(pack(self.data_format, *data))
+
+    def get_value(self):
+        value = self.get_data()[:-1]
+        # If only a single value, just return the value rather than a tuple
+        return value if len(value) > 1 else value[0] 
+
+    def get_next(self):
+        next = self.get_data()[-1]
+        if next != 0:
+            return PersistentNode(self.file_object, self.value_format, address=next)
+        return None
+
+    def get_data(self):
+        self.file_object.seek(self.address - self.data_length)
+        bytes = self.file_object.read(self.data_length)
+        return list(unpack(self.data_format, bytes))
+
+    def __str__(self):
+        next = self.get_next()
+        return "{value : %s, next : %s}"%(self.get_value(), next.address if next is not None else None)
---
+++ PersistentList/persistent_walkthrough.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+import os
+from itertools import izip
+from persistent_list import PersistentList
+
+######################################################################
+# Here is our persistent integer list
+# Each node in the list stores an array of integers, the default
+# is just one integer
+######################################################################
+class IntegerList(PersistentList):
+    def __init__(self, file_object, number_of_ints=1, address=0):
+        PersistentList.__init__(self, file_object, "I"*number_of_ints,
+                address=address)
+
+######################################################################
+# Now we'll run through a couple simple use cases for 
+# using the persistent list
+######################################################################
+filename = "test.db"
+
+# Create the file if it doesn't exist
+if not os.path.exists(filename): 
+    open(filename, 'w').close()
+
+db = open(filename, "r+b")
+
+######################################################################
+# Create three individual lists, but all will be stored in the
+# same file. One list will store even numbers, one will store
+# odd numbers, and one will store tuples of (even, odd) pairs.
+######################################################################
+evenList    = IntegerList(db)
+oddList     = IntegerList(db)
+evenOddList = IntegerList(db, 2) # Stores 2 ints instead of 1
+
+######################################################################
+# First we'll populate our even and odd lists
+######################################################################
+for i in xrange(10):
+    if i%2 == 0:
+        evenList.append(i)
+    else:
+        oddList.append(i)
+
+######################################################################
+# We zip the two lists together, and populate our third list
+######################################################################
+for i in izip(evenList, oddList):
+    evenOddList.append(*i)
+
+######################################################################
+# Oh no! We closed the file! Wherever are our lists!?
+######################################################################
+db.close()
+
+######################################################################
+# As long as we have the address of the head node in the list, we're
+# okay. We need the head address because the node can be anywhere in
+# the file.
+######################################################################
+evenHead    = evenList.head.address
+oddHead     = oddList.head.address
+evenOddHead = evenOddList.head.address
+
+######################################################################
+# Open the file again, we're going to prove those lists are in there
+# somewhere.
+######################################################################
+db = open(filename, "r+b")
+
+######################################################################
+# We create the lists again, but this time specify an address
+######################################################################
+evenList    = IntegerList(db,    address=evenHead)
+oddList     = IntegerList(db,    address=oddHead)
+evenOddList = IntegerList(db, 2, address=evenOddHead)
+
+######################################################################
+# Here's what all of our hard work produced. Whew....
+######################################################################
+print evenList
+print oddList
+print evenOddList
+
+######################################################################
+# k thx bai
+######################################################################
+db.close()
