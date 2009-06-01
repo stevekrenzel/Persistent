5cb1845be3ab54bfa9fcb095ff426f51772a4542
Steve Krenzel <sgk284@gmail.com>
2009-05-31 20:30:12 -0400

    Adds StringList and some __init__ clean up

---
+++ Persistent/StringList/string_list.py
@@ -0,0 +1,23 @@
+from Persistent import IntegerList
+
+class StringList(IntegerList):
+    def __init__(self, file_object, address=0):
+        self.file_object = file_object
+        # We make an integer list that stores two ints. One
+        # int for the address of the string, and one int for
+        # the length of the string
+        IntegerList.__init__(self, file_object, 2, address)
+
+    def append(self, value):
+        self.file_object.seek(0, 2)
+        address = self.file_object.tell()
+        length  = len(value)
+        self.file_object.write(value)
+        IntegerList.append(self, address, length)
+
+    def __iter__(self):
+        for value in self.node_iter():
+            val = value.get_value()
+            address, length = val[0], val[1]
+            self.file_object.seek(address)
+            yield self.file_object.read(length) 
