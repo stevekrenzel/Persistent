2caed2617fc9e010fc7cffbb1f6caec3858cb0f7
steve <steve@johnny.(none)>
2009-11-04 07:54:40 +0000

    Fixes order that items are gotten

--- Persistent/Hashset/hashset.py
+++ Persistent/Hashset/hashset.py
@@ -25,7 +25,7 @@ class Hashset(DynamicCollection):
         self.set(*data)
 
     def get(self, data, default=None):
-        for a in self.collections:
+        for a in reversed(self.collections):
             b = a.get(data)
             if b != None:
                 return b
@@ -33,46 +33,3 @@ class Hashset(DynamicCollection):
 
     def __contains__(self, data):
         return self.get(data) != None
-
-def main():
-    import os
-    from random import randint, seed
-    from Persistent import Data
-    from time import time
-    seed(6)
-
-    filename = "hashset_test.db"
-    rands    = set([randint(0, 50000000) for i in xrange(25000)])
-    data    = []
-    format   = "I:age, 20p:name"
-    for r in rands:
-        d = Data(format)
-        d["age"]  = r
-        d["name"] = "Steve"
-        data.append(d)
-    del(rands)
-
-    # Create the file if it doesn't exist
-    if not os.path.exists(filename):
-        open(filename, 'w').close()
-    db = open(filename, "r+b")
-
-    hashset  = Hashset(format, db)
-
-    t = time()
-    for d in data:
-        hashset.set(d)
-    print time() - t
-
-    t = time()
-    for d in data:
-        if hashset.get(d) != d:
-            print "SHIT"
-    print time() - t
-
-    db.close()
-    os.remove(filename)
-
-if __name__ == "__main__":
-    main()
-
